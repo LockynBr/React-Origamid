e9d78a2f2f77ca4955ee041fe820ced3967af539
Lockynbr <pauloluz566@gmail.com>
2024-12-18 17:06:45 -0300

    adicionando aulas de Hooks - useState

---
+++ Aula10.1/src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+function App() {
+  const [contar, setContar] = React.useState(1);
+  const [items, setItems] = React.useState(['Item 1']);
+
+  function handleClick() {
+    setContar((contar) => {
+      // setContar possui um efeito colateral.
+      setItems((items) => [...items, 'Item ' + (contar + 1)]);
+      return contar + 1;
+    });
+    // Tirar o efeito de dentro do setContar irá concertar o erro
+    // setItems((items) => [...items, 'Item ' + (contar + 1)]);
+  }
+
+  return (
+    <>
+      {items.map((item) => (
+        <li key={item}>{item}</li>
+      ))}
+      <button onClick={handleClick}>{contar}</button>
+    </>
+  );
+}
+
+export default App;
