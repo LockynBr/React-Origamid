37cfaaaa2b3fd85862c9ebd8dc5a0daa8faad1a5
Lockynbr <pauloluz566@gmail.com>
2025-01-10 16:10:04 -0300

    exercicio de input

---
+++ Exercicios/Ex07/src/App.jsx
@@ -0,0 +1,96 @@
+import React from 'react';
+
+const formFields = [
+  {
+    id: 'nome',
+    label: 'Nome',
+    type: 'text',
+  },
+  {
+    id: 'email',
+    label: 'Email',
+    type: 'email',
+  },
+  {
+    id: 'senha',
+    label: 'Senha',
+    type: 'password',
+  },
+  {
+    id: 'cep',
+    label: 'Cep',
+    type: 'text',
+  },
+  {
+    id: 'numero',
+    label: 'Numero',
+    type: 'text',
+  },
+  {
+    id: 'bairro',
+    label: 'Bairro',
+    type: 'text',
+  },
+  {
+    id: 'cidade',
+    label: 'Cidade',
+    type: 'text',
+  },
+  {
+    id: 'estado',
+    label: 'Estado',
+    type: 'text',
+  },
+];
+
+function App() {
+  const [form, setForm] = React.useState(
+    formFields.reduce((acc, field) => {
+      return {
+        ...acc,
+        [field.id]: '',
+      };
+    }),
+  );
+  const [response, setResponse] = React.useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch('https://ranekapi.origamid.dev/json/api/usuario', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(form),
+    }).then((response) => {
+      setResponse(response);
+    });
+  }
+
+  function handleChange({ target }) {
+    const { id, value } = target;
+    setForm({ ...form, [id]: value });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {formFields.map(({ id, label, type }) => {
+        return (
+          <div key={id}>
+            <label htmlFor={id}>{label}</label>
+            <input
+              type={type}
+              id={id}
+              value={form[id]}
+              onChange={handleChange}
+            />
+          </div>
+        );
+      })}
+      {response && response.ok && <p>Usuário Criado com Sucesso!</p>}
+      <button style={{ marginTop: '20px' }}>Enviar</button>
+    </form>
+  );
+}
+
+export default App;
